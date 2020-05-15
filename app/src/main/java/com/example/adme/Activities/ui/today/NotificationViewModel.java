676855b9ffb5b19554f371c4d32dc8b7f221c22d
Sheersho  Mueez <mueezsheersho95@gmail.com>
2020-05-15 21:20:04 +0600

    mueez1

---
+++ app/src/main/java/com/example/adme/Activities/ui/today/NotificationViewModel.java
@@ -0,0 +1,7 @@
+package com.example.adme.Activities.ui.today;
+
+import androidx.lifecycle.ViewModel;
+
+public class NotificationViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/adme/Activities/ui/today/Notification_Fragment.java
@@ -0,0 +1,38 @@
+package com.example.adme.Activities.ui.today;
+
+import androidx.lifecycle.ViewModelProviders;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.adme.R;
+
+public class Notification_Fragment extends Fragment {
+
+    private NotificationViewModel mViewModel;
+
+    public static Notification_Fragment newInstance() {
+        return new Notification_Fragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.notification__fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(NotificationViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
